0d4177bdc65a17d35fc47da88acf9124b6d00090
starfirst <starfirst57@gmail.com>
2021-08-08 06:02:50 +0700

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greetings.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greetings {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea = idea + " But i newbie ";
+        int year = 2021;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
