c6205ae35f0ccfc8ea065029007d212b943d1a04
starfirst <starfirst57@gmail.com>
2021-08-12 18:04:50 +0700

    6.7.2. Моно столбец в матрице. [#214127 #188932]

--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -29,6 +29,18 @@ public class MatrixCheckTest {
     }
 
     @Test
+    public void whenHasNotMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void whenDiagonalFullX() {
         char[][] input = {
                 {'X', ' ', ' '},
